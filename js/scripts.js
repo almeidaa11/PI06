cb56d45e96a97b3166588c2eee887e86ea84a90a
Bruno Almeida <bruno.ag@sempreceub.com>
2021-09-27 14:52:10 -0300

    team

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,5 @@
 $(document).ready(function(){
 
-    // Progress bar
-    let containerA = document.getElementById("circleA");
-
-    let circleA = new ProgressBar.Circle(containerA, {
-
-        color: '#64DAF9',
-        stroke: 8,
-        duration: 1400,
-        from: {color: '#AAA'},
-        to: {color: '#65DAF9'},
-
-        step: function(state, circle){
-            circle.path.setAttribute('stroke', state.color);
-
-            var value = Math.round(circle.value() * 1);
-            circle.setText(value);
-        }
-    });
-
-
+   
 
 });
