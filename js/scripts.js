dffc55a654184cf15fc5a09a80abca522159ec60
Bruno Almeida <bruno.ag@sempreceub.com>
2021-09-27 15:40:33 -0300

    finish

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,38 @@
-$(document).ready(function(){
-
-   
-
-});
+$( document ).ready(function() {
+  
+    // scroll para as seções
+  
+    let navBtn = $('.nav-item');
+  
+    let homeSection = $('#homeSlider');
+    let aboutSection = $('#about-area');
+    let ferramentasSection = $('#service-area')
+    let teamSection = $('#team-area');
+    let contactSection = $('#contact-area');
+  
+    let scrollTo = '';
+  
+    $(navBtn).click(function() {
+  
+      let btnId = $(this).attr('id');
+  
+      if(btnId == 'home-menu') {
+        scrollTo = homeSection;
+      } else if(btnId == 'about-menu') {
+        scrollTo = aboutSection;
+      }else if(btnId == 'service-menu') {
+        scrollTo = ferramentasSection;
+      } else if(btnId == 'team-menu') {
+        scrollTo = teamSection;
+      } else if(btnId == 'contact-menu') {
+        scrollTo = contactSection;
+      } else {
+        scrollTo = homeSection;
+      }
+  
+      $([document.documentElement, document.body]).animate({
+          scrollTop: $(scrollTo).offset().top - 70
+      }, 1500);
+    });
+  
+  });
